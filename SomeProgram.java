32d1da1f373b74fe29d0f8b51f81291dd2513073
Monika Orlowska <morlowska@student.agh.edu.pl>
2025-01-18 15:21:25 +0100

    Start to implement. Phase2 rocket

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,4 +3,5 @@ public class SomeProgram {
       System.out.println("We are learning to use Git.");
       System.out.println("We will be Git experts, soon.");
     }
+    private static int add(int 5, int 4) {}
 }
